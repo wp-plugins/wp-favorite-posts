470d8f2676bbec858ab27f4aadd0f9dc13da53db
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-23 13:36:02 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/tags/1.0@96862 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== WP Favorite Posts ===
+Contributors: Hüseyin Berberoğlu
+Donate link: http://birazkisisel.com
+Tags: favorite, posts, favorites, cookie, wp-favorite-posts
+Requires at least: 2.0.2
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+Allows users to add favorite posts. This plugin use cookies for saving data so
+unregistered users can favorite a post. There is no register requirement.
+
+== Description ==
+
+Allows users to add favorite posts. This plugin use cookies for saving data so
+unregistered users can favorite a post. There is no register requirement.
+
+== Installation ==
+
+1. Unzip into your `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php wpfp_link(); ?>` in your single.php template
+file. Then favorite this post link will appear in all posts.
+1. Create a page e.g. "Your Favorites" and insert `{{wp-favorite-posts}}`
+text into content section. This page will contain users favorite posts.
+1. That's it :)
---
+++ wpfp-admin.php
@@ -0,0 +1,56 @@
+<?php
+if ( isset($_POST['submit']) ) {
+	if ( function_exists('current_user_can') && !current_user_can('manage_options') )
+		die(__('Cheatin&#8217; uh?'));
+	$wpfp_options = array();
+	$wpfp_options['add_favorite'] = addslashes($_POST['add_favorite']);
+	$wpfp_options['added'] = addslashes($_POST['added']);
+	$wpfp_options['remove_favorite'] = addslashes($_POST['remove_favorite']);
+	$wpfp_options['removed'] = addslashes($_POST['removed']);
+	$wpfp_options['clear'] = addslashes($_POST['clear']);
+	$wpfp_options['cleared'] = addslashes($_POST['cleared']);
+	$wpfp_options['favorites_empty'] = addslashes($_POST['favorites_empty']);
+	$wpfp_options['cookie_warning'] = addslashes($_POST['cookie_warning']);
+
+	update_option('wpfp_options', $wpfp_options);
+}
+$wpfp_options = get_option('wpfp_options');
+?>
+<?php if ( !empty($_POST ) ) : ?>
+<div id="message" class="updated fade"><p><strong><?php _e('Options saved.', 'wpfp') ?></strong></p></div>
+<?php endif; ?>
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2><?php _e('Favorite Posts Configuration', 'wp-favorite-posts'); ?></h2>
+<form action="" method="post">
+<table class="form-table">
+	<tr>
+		<th><?php _e("Text for add link", "wp-favorite-posts") ?></th><td><input type="text" name="add_favorite" value="<?php echo stripslashes($wpfp_options['add_favorite']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for added", "wp-favorite-posts") ?></th><td><input type="text" name="added" value="<?php echo stripslashes($wpfp_options['added']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for remove link", "wp-favorite-posts") ?></th><td><input type="text" name="remove_favorite" value="<?php echo stripslashes($wpfp_options['remove_favorite']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for removed", "wp-favorite-posts") ?></th><td><input type="text" name="removed" value="<?php echo stripslashes($wpfp_options['removed']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for clear link", "wp-favorite-posts") ?></th><td><input type="text" name="clear" value="<?php echo stripslashes($wpfp_options['clear']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for cleared", "wp-favorite-posts") ?></th><td><input type="text" name="cleared" value="<?php echo stripslashes($wpfp_options['cleared']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for favorites are", "wp-favorite-posts") ?> empty</th><td><input type="text" name="favorites_empty" value="<?php echo stripslashes($wpfp_options['favorites_empty']); ?>" /></td>
+	</tr>
+	<tr>
+		<th><?php _e("Text for favorites saved to cookies", "wp-favorite-posts") ?></th><td><textarea name="cookie_warning"><?php echo stripslashes($wpfp_options['cookie_warning']); ?></textarea></td>
+	</tr>
+	<tr>
+		<th></th><td><input type="submit" name="submit" class="button" value="<?php e_("Save Changes", "wp-favorite-posts") ?>" /></td>
+	</tr>
+</table>
+</form>
+</div>
