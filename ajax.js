fc8c8d01acda234faf15c6ae7a8a1e1013d7c0d7
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-24 08:47:12 +0000

    use params, dont need to split
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/trunk@97164 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax.js
+++ ajax.js
@@ -5,15 +5,7 @@ jQuery(document).ready( function($) {
     url = document.location.href;
     params = $(this).attr("href");
     params = params.replace("?", "");
-    params = params.split("&");
-    fav = params[0].split("=")[1];
-    post = params[1].split("=")[1];
-    url = params[2].split("=")[1];
-    $.get(url, {
-            favorite: fav,
-            post: post,
-            url: url
-        }, function(data) {
+    $.get(url, params, function(data) {
 	    $("#wpfp-loading").removeClass("wpfp-showloading");
 	    $("#wpfp-loading").addClass("wpfp-hideloading");
             $("#wpfp-span").html(data);
