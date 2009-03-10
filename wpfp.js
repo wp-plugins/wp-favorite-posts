8bfe08d6a3d0fa343c4200b5eb49570be3a3c760
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-10 15:22:32 +0000

    fix duplicate loading image problem
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/trunk@101022 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpfp.js
+++ wpfp.js
@@ -1,15 +1,15 @@
 jQuery(document).ready( function($) {
   $(".wpfp-link").click( function() {
     dataBack = $(this).parent();
-    $(this).prepend($(".wpfp-loading"));
-    $(".wpfp-loading").removeClass("wpfp-hideloading");
-    $(".wpfp-loading").addClass("wpfp-showloading");
+    loadingImg = $(this).prev();
+    loadingImg.removeClass("wpfp-hideloading");
+    loadingImg.addClass("wpfp-showloading");
     url = document.location.href;
     params = $(this).attr("href");
     params = params.replace("?", "");
     $.get(url, params, function(data) {
-	    $(".wpfp-loading").removeClass("wpfp-showloading");
-	    $(".wpfp-loading").addClass("wpfp-hideloading");
+            loadingImg.removeClass("wpfp-showloading");
+            loadingImg.addClass("wpfp-hideloading");
             dataBack.html(data);
         }
     );
