c4101030983537687977870b2b591d3ba8e5f179
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-21 12:12:54 +0000

    tagged 1.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/tags/1.4@220328 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-favorite-posts.php
+++ wp-favorite-posts.php
@@ -4,7 +4,7 @@
 Plugin Name: WP Favorite Posts
 Plugin URI: http://nxsn.com/my-projects/wp-favorite-posts-plugin/
 Description: Allows users to add favorite posts. This plugin use cookies for saving data so unregistered users can favorite a post. Put <code>&lt;?php wpfp_link(); ?&gt;</code> where ever you want on a single post. Then create a page which includes that text : <code>{{wp-favorite-posts}}</code> That's it!
-Version: 1.3.5
+Version: 1.4
 Author: Hüseyin Berberoğlu
 Author URI: http://nxsn.com
 
