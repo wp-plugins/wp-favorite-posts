ded42e0af74ca0df5dd8a9c892248c396cdf0383
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-27 22:04:29 +0000

    single quotes
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/trunk@222119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpfp.js
+++ wpfp.js
@@ -1,16 +1,15 @@
 jQuery(document).ready( function($) {
-  $(".wpfp-link").live('click', function() {
+  $('.wpfp-link').live('click', function() {
     dataBack = $(this).parent();
     loadingImg = $(this).prev();
     beforeImg = $(this).prev().prev();
-    beforeImg.addClass("wpfp-hide");
-    loadingImg.removeClass("wpfp-hide");
-    loadingImg.addClass("wpfp-showloading");
+    beforeImg.addClass('wpfp-hide');
+    loadingImg.removeClass('wpfp-hide');
+    loadingImg.addClass('wpfp-showloading');
     url = document.location.href.split('#')[0];
-    params = $(this).attr("href").replace('?', '') + "&ajax=1";
+    params = $(this).attr('href').replace('?', '') + '&ajax=1';
     $.get(url, params, function(data) {
-            loadingImg.removeClass("wpfp-showloading");
-    //        loadingImg.addClass("wpfp-hide");
+            loadingImg.removeClass('wpfp-showloading');
             dataBack.html(data);
         }
     );
