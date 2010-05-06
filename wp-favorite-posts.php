06c54be887d95ddd43043b3182cacf377bd362e9
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-06 19:18:01 +0000

    new widget design
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/trunk@238067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-favorite-posts.php
+++ wp-favorite-posts.php
@@ -335,53 +335,8 @@ function wpfp_list_most_favorited($limit=5) {
         echo "</ul>";
     }
 }
-function wpfp_widget_init() {
-    function wpfp_widget_view($args) {
-        extract($args);
-        $options = wpfp_get_options();
-        if (isset($options['widget_limit'])) {
-            $limit = $options['widget_limit'];
-        }
-        $title = empty($options['widget_title']) ? 'Most Favorited Posts' : $options['widget_title'];
-        echo $before_widget;
-        echo $before_title
-             . $title
-             . $after_title;
-        wpfp_list_most_favorited($limit);
-        echo $after_widget;
-    }
-    function wpfp_widget_control() {
-        $options = wpfp_get_options();
-        if (isset($_POST["wpfp-widget-submit"])):
-            $options['widget_title'] = strip_tags(stripslashes($_POST['wpfp-title']));
-            $options['widget_limit'] = strip_tags(stripslashes($_POST['wpfp-limit']));
-            update_option("wpfp_options", $options);
-        endif;
-        $title = $options['widget_title'];
-        $limit = $options['widget_limit'];
-    ?>
-        <p>
-            <label for="wpfp-title">
-                <?php _e('Title:'); ?> <input type="text" value="<?php echo $title; ?>" class="widefat" id="wpfp-title" name="wpfp-title" />
-            </label>
-        </p>
-        <p>
-            <label for="wpfp-limit">
-                <?php _e('Number of posts to show:'); ?> <input type="text" value="<?php echo $limit; ?>" style="width: 28px; text-align:center;" id="wpfp-limit" name="wpfp-limit" />
-            </label>
-        </p>
-        <?php if (!$options['statics']) { ?>
-        <p>
-            You must enable statics from favorite posts <a href="plugins.php?page=wp-favorite-posts" title="Favorite Posts Configuration">configuration page</a>.
-        </p>
-        <?php } ?>
-        <input type="hidden" name="wpfp-widget-submit" value="1" />
-    <?php
-    }
-    register_sidebar_widget('Most Favorited Posts', 'wpfp_widget_view');
-    register_widget_control('Most Favorited Posts', 'wpfp_widget_control' );
-}
-add_action('widgets_init', 'wpfp_widget_init');
+
+include("wpfp-widgets.php");
 
 //---\\
 function wpfp_get_cookie() {
