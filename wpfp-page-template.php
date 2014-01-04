0997ca7ba1adb1ebe7718c7409887de42fc5633f
hberberoglu <hberberoglu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-04 12:14:07 +0000

    version bump
    git-svn-id: https://plugins.svn.wordpress.org/wp-favorite-posts/trunk@832779 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpfp-page-template.php
+++ wpfp-page-template.php
@@ -1,15 +1,16 @@
 <?php
+    $wpfp_before = "";
     echo "<div class='wpfp-span'>";
-    if (!empty($user)):
-        if (!wpfp_is_user_favlist_public($user)):
-            echo "$user's Favorite Posts.";
-        else:
-            echo "$user's list is not public.";
-        endif;
-    endif;
+    if (!empty($user)) {
+        if (wpfp_is_user_favlist_public($user)) {
+            $wpfp_before = "$user's Favorite Posts.";
+        } else {
+            $wpfp_before = "$user's list is not public.";
+        }
+    }
 
     if ($wpfp_before):
-        echo "<p>".$wpfp_before."</p>";
+        echo '<div class="wpfp-page-before">'.$wpfp_before.'</div>';
     endif;
 
     echo "<ul>";
@@ -17,7 +18,12 @@
 		$favorite_post_ids = array_reverse($favorite_post_ids);
         $post_per_page = wpfp_get_option("post_per_page");
         $page = intval(get_query_var('paged'));
-        query_posts(array('post__in' => $favorite_post_ids, 'posts_per_page'=> $post_per_page, 'orderby' => 'post__in', 'paged' => $page));
+
+        $qry = array('post__in' => $favorite_post_ids, 'posts_per_page'=> $post_per_page, 'orderby' => 'post__in', 'paged' => $page);
+        // custom post type support can easily be added with a line of code like below.
+        // $qry['post_type'] = array('post','page');
+        query_posts($qry);
+
         while ( have_posts() ) : the_post();
             echo "<li><a href='".get_permalink()."' title='". get_the_title() ."'>" . get_the_title() . "</a> ";
             wpfp_remove_favorite_link(get_the_ID());
